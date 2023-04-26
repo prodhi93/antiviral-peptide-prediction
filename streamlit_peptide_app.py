482e635955649da6a2828ae0aac8f18350188192
prodhi93 <118654457+prodhi93@users.noreply.github.com>
2023-04-27 02:55:25 +0300

    Update streamlit_peptide_app.py

--- streamlit_peptide_app.py
+++ streamlit_peptide_app.py
@@ -9,23 +9,26 @@ from antiviral_analysis.peptide_class import Peptide
 
 st.title("Antiviral Peptide Predictor")
 
-st.subheader("Enter the amino acid sequence of your peptide")
-seq = st.text_input('Enter sequence')
+with st.form(key="peptide_choices"):
+    st.subheader("Enter the amino acid sequence of your peptide")
+    seq = st.text_input('Enter sequence')
 
-#Picking which model user wants to deploy
-st.subheader("Is your peptide ribosomally synthesised (biologically occurring), or synthetic?")
-synth_type = st.radio('Synthesis Type',('Ribosomal', 'Synthetic')) 
-#option is stored in this variable
+    #Picking which model user wants to deploy
+    st.subheader("Is your peptide ribosomally synthesised (biologically occurring), or synthetic?")
+    synth_type = st.radio('Synthesis Type',('Ribosomal', 'Synthetic')) 
+    #option is stored in this variable
 
-st.subheader("Choose your threshold for positive antiviral activity")
-threshold = st.slider("Percent threshold", min_value=0.01, max_value=100.00, step=0.01, \
-                      help="With an extreme needle-in-haystack problem such as this one, positive probabilities are low despite undersampling correction.\
-                     Hint: 25% is a good starter value for positivity thresholds as the model returns probability values > 0.25 for confirmed antiviral peptides.")
-threshold = float(threshold)/100
+    st.subheader("Choose your threshold for positive antiviral activity")
+    threshold = st.slider("Percent threshold", min_value=0.01, max_value=100.00, step=0.01, \
+                          help="With an extreme needle-in-haystack problem such as this one, positive probabilities are low despite undersampling correction.\
+                         Hint: 25% is a good starter value for positivity thresholds as the model returns probability values > 0.25 for confirmed antiviral peptides.")
+    threshold = float(threshold)/100
 
-st.subheader("Choose the number of amino acids in the shortest truncated segment")
-min_length = st.slider("Minimum length", min_value=1, max_value=len(seq), step=1)
-min_length = int(min_length)
+    st.subheader("Choose the number of amino acids in the shortest truncated segment")
+    min_length = st.slider("Minimum length", min_value=1, max_value=len(seq), step=1)
+    min_length = int(min_length)
+    
+    st.form_submit_button("Enter")
 
 @st.cache(allow_output_mutation=True)
 def model_loader(model_name_or_path):
@@ -81,27 +84,27 @@ def customise_aggrid_df(data):
     df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
     return df
 
+if form_submit_button:
+    rfc_full = model_loader("smote_rfc_full.joblib")
+    if synth_type=="Synthetic":
+        rfc_synth = model_loader("smote_rfc_synth.joblib")
+        seq_preds_synth = get_truncated_data(seq, rfc_synth, min_length) 
+    seq_preds_full = get_truncated_data(seq, rfc_full, min_length)
 
-rfc_full = model_loader("smote_rfc_full.joblib")
-if synth_type=="Synthetic":
-    rfc_synth = model_loader("smote_rfc_synth.joblib")
-    seq_preds_synth = get_truncated_data(seq, rfc_synth, min_length) 
-seq_preds_full = get_truncated_data(seq, rfc_full, min_length)
-    
 
-st.header("Prediction Tables")
+    st.header("Prediction Tables")
 
-if synth_type=="Synthetic":
-    df = pd.DataFrame(seq_preds_synth[0], columns=["Sequence"])
-    df["Predicted Antiviral Probability"] = seq_preds_synth[2]
-    df["Possible Antiviral?"] = df["Predicted Antiviral Probability"].apply(lambda x: "Yes" if x > threshold else "No" )
+    if synth_type=="Synthetic":
+        df = pd.DataFrame(seq_preds_synth[0], columns=["Sequence"])
+        df["Predicted Antiviral Probability"] = seq_preds_synth[2]
+        df["Possible Antiviral?"] = df["Predicted Antiviral Probability"].apply(lambda x: "Yes" if x > threshold else "No" )
 
-    st.subheader(f"Using model trained on {synth_type} Peptides")
-    customise_aggrid_df(df)
+        st.subheader(f"Using model trained on {synth_type} Peptides")
+        customise_aggrid_df(df)
 
-df_full = pd.DataFrame(seq_preds_full[0], columns=["Sequence"])
-df_full["Predicted Antiviral Probability"] = seq_preds_full[2]
-df_full["Possible Antiviral?"] = df_full["Predicted Antiviral Probability"].apply(lambda x: "Yes" if x > threshold else "No" )
+    df_full = pd.DataFrame(seq_preds_full[0], columns=["Sequence"])
+    df_full["Predicted Antiviral Probability"] = seq_preds_full[2]
+    df_full["Possible Antiviral?"] = df_full["Predicted Antiviral Probability"].apply(lambda x: "Yes" if x > threshold else "No" )
 
-st.subheader(f"Using model trained on both Ribosomal and Synthetic Peptides")
-customise_aggrid_df(df_full)
+    st.subheader(f"Using model trained on both Ribosomal and Synthetic Peptides")
+    customise_aggrid_df(df_full)
